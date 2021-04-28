246afb6a96df38695fbfe1f83ac1b78427291a00
Máté Kerekes <everydaymadnes@gmail.com>
2021-04-28 21:25:11 +0200

    Fix: Better dev shell experience; container stays open after server dies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-set +e
-./start_pr.sh +multi 1 +dedicated 1 $@
-echo ""
-echo "Server stopped; exit the shell to clean up..."
-echo ""
-bash
+bash -i <<EOF
+./start_pr.sh +multi 1 +dedicated 1
+echo "Server stopped. Exit shell to clean up."
+exec </dev/tty
+EOF
