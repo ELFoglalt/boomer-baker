6f5c88ffc879890129b02288f6e50320a74f6d45
Máté Kerekes <everydaymadnes@gmail.com>
2021-04-29 22:07:35 +0200

    Fix: Fixed entrypoint passing lines to server console

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-bash -i <<EOF
+set +e
+read -r -d '' RUN_COMMANDS <<-'EOF'
 ./start_pr.sh +multi 1 +dedicated 1
 echo "Server stopped. Exit shell to clean up."
 exec </dev/tty
 EOF
+
+bash -ic $RUN_COMMANDS
