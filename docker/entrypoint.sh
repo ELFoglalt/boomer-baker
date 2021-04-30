15a5fadd03dc9633182fd45538e6dda8fa565dd8
Máté Kerekes <everydaymadnes@gmail.com>
2021-04-30 16:45:57 +0200

    Fix: Fixing the entryscript to keep bash open *again*
    This time it actually works I think.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
-set +e
-read -r -d '' RUN_COMMANDS <<-'EOF'
-./start_pr.sh +multi 1 +dedicated 1
-echo "Server stopped. Exit shell to clean up."
-exec </dev/tty
-EOF
 
-bash -ic $RUN_COMMANDS
+./start_pr.sh +multi 1 +dedicated 1
+ echo "Server stopped. Exit shell to clean up docker container, or run the server again as needed."
+ history -s "./start_pr.sh +multi 1 +dedicated 1"
+ HISTCONTROL=ignorespace
